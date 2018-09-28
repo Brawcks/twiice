737479c870f46766638fdcea9103dc91ebd85be6
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-09-29 00:13:58 +0200

    [ADD] Routes for Tpee

--- client/standard/tpee/main.js
+++ client/standard/tpee/main.js
@@ -1,5 +1,7 @@
 // LOAD ALL TEMPLATES FIRST
 import './templates/tpeeHome.html';
+import './templates/views/sub_module.html';
+import './templates/views/settings.html';
 
 // LOAD ALL JS FILES
 import './static/js/functions/functions.js';

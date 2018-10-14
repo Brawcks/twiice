de2cc09ac826eadec2b5b0ff5cd6086478c162d8
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-10-14 22:11:53 +0200

    [ADD] Server side folders for tpee module

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
+
+import './standard/main.js';
+import './z-customer/main.js';
---
+++ server/standard/main.js
@@ -0,0 +1,1 @@
+import './tpee/main.js';

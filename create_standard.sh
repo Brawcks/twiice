513a56c0b15c0d09036990870ee9210aa42fc66d
tiktakweb <vincent@tiktakweb.fr>
2018-11-16 20:01:51 +0100

    [IMP] Improve modules scripts generation for collections

--- create_standard.sh
+++ create_standard.sh
@@ -7,7 +7,6 @@ mkdir client/standard/$module_name
 mkdir client/standard/$module_name/templates
 mkdir client/standard/$module_name/templates/views
 mkdir client/standard/$module_name/static
-mkdir client/standard/$module_name/collections
 mkdir client/standard/$module_name/static/js
 mkdir client/standard/$module_name/static/js/data
 mkdir client/standard/$module_name/static/js/functions
@@ -124,3 +123,17 @@ echo "Meteor.startup(() => {
 });" > server/standard/$module_name/main.js
 
 printf "\nimport './$module_name/main.js';" >> server/standard/main.js
+
+# COLLECTIONS DIRECTORIES
+
+mkdir collections/standard/$module_name
+
+touch collections/standard/$module_name/collection.js
+
+printf "\nimport './$module_name/collection.js';" >> collections/standard/main.js
+
+echo "You module is now ready."
+echo "You can now create your collections in /collections/standard/$module_name/collection.js"
+echo "Don't forget to load them on the server side, in this file : /server/standard/$module_name/main.js"
+
+echo "This script was built by Vincent Coffin - Twiice Corp."

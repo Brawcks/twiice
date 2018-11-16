13f670ed783192ab6056ecfa6a2f8a7ebcef5ebd
tiktakweb <vincent@tiktakweb.fr>
2018-11-16 23:00:18 +0100

    [ADD] Single pipeline for crm

--- client/standard/crm/main.js
+++ client/standard/crm/main.js
@@ -1,11 +1,13 @@
 // LOAD ALL TEMPLATES FIRST
 import './templates/crm.html';
+import './templates/crm_single_pipeline.html';
 import './templates/views/sub_module.html';
 import './templates/views/settings.html';
 
 // LOAD ALL JS FILES
 import './static/js/functions/functions.js';
 import './static/js/crm.js';
+import './static/js/crm_single_pipeline.js';
 
 // LOAD COLLECTIONS
 // import './collections/crm.js';

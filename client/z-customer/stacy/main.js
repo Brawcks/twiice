16d5459c19a942256774ab90fcfe0650e493e1e9
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-11-17 16:59:40 +0100

    [REM] Remove stacy's specific module

--- client/z-customer/stacy/main.js
+++
@@ -1,8 +0,0 @@
-// LOAD ALL TEMPLATES FIRST
-import './templates/stacy.html';
-import './templates/views/sub_module.html';
-import './templates/views/settings.html';
-
-// LOAD ALL JS FILES
-import './static/js/functions/functions.js';
-import './static/js/stacy.js';

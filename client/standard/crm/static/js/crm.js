9c24bf5d255ab786e2816f75494821eab7fc9400
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-11-18 22:28:18 +0100

    [WIP] Start import of csv file

--- client/standard/crm/static/js/crm.js
+++ client/standard/crm/static/js/crm.js
@@ -37,8 +37,21 @@ Template.crmTreeView.events({
     'click .export-csv': function (events, template) {
         var data = Papa.unparse(Pipelines.find({}, { limit: 10 }).fetch());
         var date = new Date().toISOString().slice(0, 10);
-        Meteor.call('download_csv', data, 'crm_'+date+'.csv', 'text/csv;encoding:utf-8');
+        Meteor.call('download_csv', data, 'crm_' + date + '.csv', 'text/csv;encoding:utf-8');
         swal("Yeah !", "Your CSV document is available !", "success");
+    },
+    'click .import-csv': function (events, template) {
+        swal("Ooops !", "This function is not available yet !", "info");
+        // $(".import-csv-file").click();
+        // $(".import-csv-file").change(function () {
+        //     var fileInput = document.querySelector('.import-csv-file');
+        //     var reader = new FileReader();
+        //     reader.addEventListener('load', function () {
+        //         alert('Contenu du fichier "' + fileInput.files[0].name + '" :\n\n' + reader.result);
+        //     });
+        //     reader.readAsText(fileInput.files[0]);
+        //     console.log(Papa.parse(reader.result, {delimiter: ";"}));
+        // });
     },
 });
 

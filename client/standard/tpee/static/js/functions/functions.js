e5bca5df7f081f0620f6b9db30b873b4da6d0d3b
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-11-18 20:27:55 +0100

    [ADD] CSV export of data for CRM, create function for all modules

--- client/standard/tpee/static/js/functions/functions.js
+++ client/standard/tpee/static/js/functions/functions.js
@@ -7,9 +7,9 @@ var publicSettings = Meteor.settings.public;
 // THIS FUNCTION SHOULD MOVE TO CLIENT STANDARD FOLDER
 export function mainTopNavbarStandard() {
     var menuStandard = "";
-    $.each(publicSettings.modules.standard, function(i, item) {
+    $.each(publicSettings.modules.standard, function (i, item) {
         // console.log(item);
-        menuStandard += '<a class="dropdown-item" href="'+item.url+'">'+item.name+'</a>';
+        menuStandard += '<a class="dropdown-item" href="' + item.url + '">' + item.name + '</a>';
     });
     return menuStandard;
 }
@@ -17,9 +17,9 @@ export function mainTopNavbarStandard() {
 // THIS FUNCTION SHOULD MOVE TO CLIENT Z-CUSTOMER FOLDER
 export function mainTopNavbarCustomer() {
     var menuCustomer = "";
-    $.each(publicSettings.modules.customer, function(i, item) {
+    $.each(publicSettings.modules.customer, function (i, item) {
         // console.log(item);
-        menuCustomer += '<a class="dropdown-item" href="'+item.url+'">'+item.name+'</a>';
+        menuCustomer += '<a class="dropdown-item" href="' + item.url + '">' + item.name + '</a>';
     });
     return menuCustomer;
 }
@@ -29,8 +29,8 @@ export function leftSidebarStandard() {
     var leftSidebarData = require('../data/sidebar.json');
     var leftSidebar = "";
 
-    $.each(leftSidebarData.sidebar, function(i, item) {
-        leftSidebar += '<a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="'+item.url+'" role="tab" aria-controls="v-pills-home" aria-selected="true">'+item.name+'</a>';
+    $.each(leftSidebarData.sidebar, function (i, item) {
+        leftSidebar += '<a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="' + item.url + '" role="tab" aria-controls="v-pills-home" aria-selected="true">' + item.name + '</a>';
     });
     return leftSidebar;
 }
@@ -41,11 +41,36 @@ export function leftSidebarStandard() {
 // GLOBAL FUNCTIONS FOR ALL MODULES SHOULD BE DEFINED HERE
 
 Meteor.methods({
+    // DESIGN FEATURES / METHODS
     tw_field_decoration: function () {
         // We here decorate bootstrap input depending on twiice special classes
-        $('form *').filter(':input').each(function(){
+        $('form *').filter(':input').each(function () {
             //your code here
             console.log("a count");
         });
     },
+
+    // TECHNICAL / DATA TRANSFORMATION
+    download_csv: function (content, fileName, mimeType) {
+        // var download = function (content, fileName, mimeType) {
+        var a = document.createElement('a');
+        mimeType = mimeType || 'application/octet-stream';
+
+        if (navigator.msSaveBlob) { // IE10
+            navigator.msSaveBlob(new Blob([content], {
+                type: mimeType
+            }), fileName);
+        } else if (URL && 'download' in a) { //html5 A[download]
+            a.href = URL.createObjectURL(new Blob([content], {
+                type: mimeType
+            }));
+            a.setAttribute('download', fileName);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        } else {
+            location.href = 'data:application/octet-stream,' + encodeURIComponent(content); // only this mime type is supported
+        }
+        // download(csvContent, 'dowload.csv', 'text/csv;encoding:utf-8');
+    }
 });

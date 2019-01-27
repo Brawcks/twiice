d1c8c2017080552a746891650524731babd205f7
antoineltc <antoineletroadec50@gmail.com>
2019-01-27 21:05:44 +0100

    [ADD] Project module

---
+++ client/standard/project/main.js
@@ -0,0 +1,9 @@
+// LOAD ALL TEMPLATES FIRST
+import './templates/project.html';
+import './templates/views/sub_module.html';
+import './templates/views/settings.html';
+
+// LOAD ALL JS FILES
+import './static/js/functions/functions.js';
+import './static/js/project_single.js';
+import './static/js/project.js';

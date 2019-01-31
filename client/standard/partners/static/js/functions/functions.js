ec9ad499fa1529b14f8b686018fa9c1831bbe949
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2019-01-31 07:57:26 +0100

    [ADD] Partners module

---
+++ client/standard/partners/static/js/functions/functions.js
@@ -0,0 +1,37 @@
+export function leftSidebarCustomer() {
+    // SIDEBAR
+    var leftSidebarData = require('../data/sidebar.json');
+    var leftSidebar = "";
+
+    $.each(leftSidebarData.sidebar, function(i, item) {
+        leftSidebar += '<a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="'+item.url+'" role="tab" aria-controls="v-pills-home" aria-selected="true">'+item.name+'</a>';
+    });
+    return leftSidebar;
+}
+
+export function filter_operator(operator, filter, val) {
+    var complete_filter = {};
+    switch (operator) {
+        case "equalTo":
+            complete_filter = {[filter]: { $eq: val } };
+            break;
+        case "isDifferentFrom":
+            complete_filter = {[filter]: { $ne: val } };
+            break
+        case "contain":
+            complete_filter = {[filter]: { $regex: '.*' + val + '.*' }};
+            break
+        case "doNotContain":
+            complete_filter = {[filter]: { $not: { $regex: val } } };
+            break
+        case "isSet":
+            complete_filter = {[filter]: { $exists: true } };
+            break
+        case "isNotSet":
+            complete_filter = {[filter]: { $exists: false } };
+            break
+        default:
+            break;
+    }
+    return complete_filter;
+}

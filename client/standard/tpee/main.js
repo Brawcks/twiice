ae4062a586a20b542154b6727c99bd727ce16066
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2019-02-03 11:42:58 +0100

    [ADD] 404 page

--- client/standard/tpee/main.js
+++ client/standard/tpee/main.js
@@ -2,6 +2,7 @@
 import './templates/main.html';
 import './templates/views/sub_module.html';
 import './templates/views/settings.html';
+import './templates/views/404.html';
 
 // LOAD ALL JS FILES
 import './static/js/functions/functions.js';
